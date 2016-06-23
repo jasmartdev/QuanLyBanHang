57447df375122d2cac682b9310bbbdcb75188d1f
Võ Quang Hòa <voquanghoa@gmail.com>
2016-06-24 02:30:41 +0700

    login

--- app/src/main/java/bk/danang/quanlybanhang/webinterface/AuthenticationService.java
+++ app/src/main/java/bk/danang/quanlybanhang/webinterface/AuthenticationService.java
@@ -6,13 +6,15 @@ import bk.danang.quanlybanhang.model.LoaiSP;
 import bk.danang.quanlybanhang.model.LoginForm;
 import bk.danang.quanlybanhang.model.LoginFormResponse;
 import retrofit.Call;
+import retrofit.http.Body;
 import retrofit.http.GET;
+import retrofit.http.POST;
 import retrofit.http.Path;
 
 /**
  * Created by voqua on 6/24/2016.
  */
 public interface AuthenticationService {
-    @GET("api/Authentication")
-    Call<LoginFormResponse> authenticate(LoginForm loginForm);
+    @POST("api/Authentication")
+    Call<LoginFormResponse> authenticate(@Body LoginForm loginForm);
 }

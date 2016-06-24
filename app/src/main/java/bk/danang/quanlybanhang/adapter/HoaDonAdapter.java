009633062556f5faecac9b22d5b52120eba8f551
Võ Quang Hòa <voquanghoa@gmail.com>
2016-06-25 01:07:09 +0700

    Fix crash

--- app/src/main/java/bk/danang/quanlybanhang/adapter/HoaDonAdapter.java
+++ app/src/main/java/bk/danang/quanlybanhang/adapter/HoaDonAdapter.java
@@ -52,7 +52,7 @@ public class HoaDonAdapter extends BaseAdapter {
         TextView tv_ten_nv = (TextView) convertView.findViewById(R.id.tv_ten_nv);
 
         tv_ten_nv.setText(NhanVienController.getInstance().findById(hoaDon.getEmployeeId())+"");
-        tv_tri_gia.setText(hoaDon.getPrice());
+        tv_tri_gia.setText(hoaDon.getPrice()+"");
         tv_ten_sp.setText(SanPhamController.getInstance().findById(hoaDon.getProductionId())+"");
 
         tv_so_luong.setText(Integer.toString(hoaDon.getQuantity()));

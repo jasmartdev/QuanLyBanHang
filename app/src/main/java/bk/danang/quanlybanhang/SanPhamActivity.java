b148249651c54082d1c2524beabd7bee9ed4d391
Võ Quang Hòa <voquanghoa@gmail.com>
2016-06-25 11:58:51 +0700

    VIP

--- app/src/main/java/bk/danang/quanlybanhang/SanPhamActivity.java
+++ app/src/main/java/bk/danang/quanlybanhang/SanPhamActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
@@ -12,9 +13,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.List;
-import java.util.Objects;
-
-import bk.danang.quanlybanhang.adapter.SpinnerAdapter;
+import bk.danang.quanlybanhang.control.ImageUrlView;
 import bk.danang.quanlybanhang.controller.LoaiSPController;
 import bk.danang.quanlybanhang.controller.NhanHieuController;
 import bk.danang.quanlybanhang.controller.PermissionController;
@@ -34,6 +33,7 @@ import retrofit.Retrofit;
 public class SanPhamActivity extends AppCompatActivity {
     private Spinner spn_loaisp, spn_hieu;
     private EditText ed_sanpham, ed_so_luong, ed_gia_goc, ed_gia_vip, ed_gia_si, ed_gia_le, ed_image_url;
+    private ImageUrlView imgPhoto;
     private int id;
     SanPham sanPham = null;
     private Retrofit retrofit = new Retrofit.Builder()
@@ -55,6 +55,8 @@ public class SanPhamActivity extends AppCompatActivity {
         ed_gia_si = (EditText) findViewById(R.id.ed_gia_si);
         ed_gia_le = (EditText) findViewById(R.id.ed_gia_le);
         ed_image_url = (EditText) findViewById(R.id.ed_image_url);
+        imgPhoto = (ImageUrlView)findViewById(R.id.sanpham_photo);
+
         Intent intent = getIntent();
         id = intent.getIntExtra("object", -1);
         if (id == -1 || !PermissionController.getInstance().getIsAdmin()) {
@@ -83,10 +85,15 @@ public class SanPhamActivity extends AppCompatActivity {
             ed_gia_si.setText(Integer.toString(sanPham.getWholesalePrice()));
             ed_gia_le.setText(Integer.toString(sanPham.getRetailPrice()));
             ed_image_url.setText(sanPham.getImageurl().toString());
+            imgPhoto.setUrl(sanPham.getImageurl());
+            imgPhoto.setVisibility(View.VISIBLE);
         }
+
         List<LoaiSP> loaiSPList = LoaiSPController.getInstance().getLoaiSPs();
         List<NhanHieu> nhanHieuList = NhanHieuController.getInstance().getNhanHieus();
-        spn_loaisp.setAdapter(new SpinnerAdapter<LoaiSP>(this, android.R.layout.simple_spinner_dropdown_item, loaiSPList));
+        spn_loaisp.setAdapter(createArrayAdapter(loaiSPList));
+        spn_hieu.setAdapter(createArrayAdapter(nhanHieuList));
+
         int loaispIndex = 0;
         int nhanhieuIndex = 0;
         if (id != -1) {
@@ -104,8 +111,15 @@ public class SanPhamActivity extends AppCompatActivity {
             }
         }
         spn_loaisp.setSelection(loaispIndex);
-        spn_hieu.setAdapter(new SpinnerAdapter<NhanHieu>(this, android.R.layout.simple_spinner_dropdown_item, nhanHieuList));
         spn_hieu.setSelection(nhanhieuIndex);
+    }
+
+    private <T> ArrayAdapter<T> createArrayAdapter(List<T> data){
+        return new ArrayAdapter<T>(this,android.R.layout.simple_spinner_dropdown_item, data);
+    }
+
+    private <T> ArrayAdapter<T> createArrayAdapter(T[] data){
+        return new ArrayAdapter<T>(this,android.R.layout.simple_spinner_dropdown_item, data);
     }
 
     public void Save(View view) {
@@ -149,6 +163,7 @@ public class SanPhamActivity extends AppCompatActivity {
         SanPhamService sanPhamService = retrofit.create(SanPhamService.class);
         SanPhamRequest sanPhamRequest = new SanPhamRequest();
         sanPhamRequest.setData(sanPham);
+        sanPhamRequest.setId(sanPham.getId());
         sanPhamRequest.setAuthentication(PermissionController.getInstance().getAuthentication());
         if (id != -1) {
             final Call<Object> call = sanPhamService.xoa(sanPhamRequest.getId(), PermissionController.getInstance().getAuthentication());
